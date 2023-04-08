f15090b2b25734b1c11d8cace3468f751d224031
LB1304 <95583840+LB1304@users.noreply.github.com>
2023-04-08 13:25:46 +0200

    Update LC.R

--- R/LC.R
+++ R/LC.R
@@ -2,17 +2,20 @@ est_LC <- function (data, k, tol_lk = 1e-8, tol_theta = 1e-8, maxit = 1e3, sv = 
                     temperingOptions = list(profile = NULL, alpha = NULL, beta = NULL, rho = NULL, T0 = NULL),
                     evolutionaryOptions = list(n_parents = NULL, n_children = NULL, prob_mut = NULL, R = NULL)) {
   
-  if (is.data.frame(data) | is.matrix(data)) {
-    data <- as.matrix(data)
-    tmp = aggr_data(data)
-    S <- tmp$S
-    yv <- tmp$freq
-  } else if (is.list(data) | identical(names(data), c("S", "yv"))) {
-    S <- data$S
-    yv <- data$yv
-  } else {
-    stop("Provide data in one of the supported format.")
-  }
+  #if (is.data.frame(data) | is.matrix(data)) {
+  #  data <- as.matrix(data)
+  #  tmp = aggr_data(data)
+  #  S <- tmp$S
+  #  yv <- tmp$freq
+  #} else if (is.list(data) | identical(names(data), c("S", "yv"))) {
+  #  S <- data$S
+  #  yv <- data$yv
+  #} else {
+  #  stop("Provide data in one of the supported format.")
+  #}
+  
+  S <- data
+  yv <- rep(1, nrow(data))
   
   # Starting Values
   if (is.null(sv)) {
