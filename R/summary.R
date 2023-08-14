c1278f62296e548bed353a6d5a3bf3fed187737a
LB1304 <95583840+LB1304@users.noreply.github.com>
2023-08-14 22:04:23 +0200

    Update summary.R

--- R/summary.R
+++ R/summary.R
@@ -20,10 +20,17 @@ summary.LC <- function(x, ...) {
 summary.HMcat <- function(x, ...) {
   cat("\nHidden Markov model for categorical data\n")
   
-  c <- dim(x$Phi)[1]; r <- dim(x$Phi)[2]; k <- dim(x$Phi)[3]; TT <- dim(x$Pi)[3]
+  c <- dim(x$Phi)[1]; r <- dim(x$Phi)[2]; k <- x$k; TT <- dim(x$V)[3]
   x$piv <- matrix(x$piv, ncol = 1)
   dimnames(x$piv) <- list("Latent State" = 1:k, "")
+  if (k == 1) {
+    x$Pi <- array(x$Pi, c(k, k, 1))
+    TT <- 1
+  }
   dimnames(x$Pi) <- list("Latent State" = 1:k, "Latent State" = 1:k, "Time Occasion" = 1:TT)
+  if (k == 1) {
+    x$Phi <- array(x$Phi, c(c, r, 1))
+  }
   dimnames(x$Phi) <- list("Category" = 0:(c-1), "Item" = 1:r, "Latent State" = 1:k)
   cat("\nInitial probabilities:\n")
   print(round(x$piv, 4))
@@ -31,7 +38,7 @@ summary.HMcat <- function(x, ...) {
   print(round(x$Pi, 4))
   cat("\nConditional response probabilities:\n")
   print(round(x$Phi, 4))
-    
+  
   cat("\nConvergence info:\n")
   print(data.frame(LogLik = x$LogLik, AIC = x$aic, BIC = x$bic, N_par = x$N_par, k = x$k, modBasic = x$modBasic, row.names = " "))
 }
@@ -39,9 +46,13 @@ summary.HMcat <- function(x, ...) {
 summary.HMcont <- function(x, ...) {
   cat("\nHidden Markov model for continuous data\n")
   
-  r <- dim(x$Mu)[1]; k <- dim(x$Mu)[2]; TT <- dim(x$Pi)[3]
+  r <- dim(x$Mu)[1]; k <- x$k; TT <- dim(x$V)[3]
   x$piv <- matrix(x$piv, ncol = 1)
   dimnames(x$piv) <- list("Latent State" = 1:k, "")
+  if (k == 1) {
+    x$Pi <- array(x$Pi, c(k, k, 1))
+    TT <- 1
+  }
   dimnames(x$Pi) <- list("Latent State" = 1:k, "Latent State" = 1:k, "Time Occasion" = 1:TT)
   dimnames(x$Mu) <- list("Item" = 1:r, "Latent State" = 1:k)
   dimnames(x$Si) <- list("Item" = 1:r, "Item" = 1:r)
@@ -64,6 +75,9 @@ summary.SB <- function(x, ...) {
   k <- length(x$piv)
   x$piv <- matrix(x$piv, ncol = 1)
   dimnames(x$piv) <- list("Latent Block" = 1:k, "")
+  if (k == 1) {
+    x$B <- matrix(x$B, ncol = k, nrow = k)
+  }
   dimnames(x$B) <- list("Latent Block" = 1:k, "Latent Block" = 1:k)
   cat("\nWeights:\n")
   print(round(x$piv, 4))
